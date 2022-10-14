390831eb90fc0665ce28de05265fcab03674d9c7
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2022-10-14 15:45:40 -0300

    exclusão código nao usado

--- project/knexFile.js
+++ project/knexFile.js
@@ -1,23 +1,3 @@
-//LOCAL
-// module.exports = {
-//     development: {
-//       client: 'postgresql',
-//       connection: {
-//         database: 'postgres',
-//         user: 'postgres',
-//         password: 'easypointz-tcc',
-//         host: '127.0.0.1',
-//         port: 5432
-//       },
-//       migrations: {
-//         directory: './src/infra/data/database/migrations',
-//         tableName: 'knex_migrations'
-//       }
-//     },
-//     staging: {},
-//     production: {}
-
-//   }
 require('dotenv').config()
 //AWS
 module.exports = {
--- project/src/infra/config/postgres.js
+++ project/src/infra/config/postgres.js
@@ -1,15 +1,3 @@
-// require('dotenv').config()
-
-// module.exports = {
-//   client: 'pg',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'postgres',
-//     password: 'easypointz-tcc',
-//     database: 'postgres'
-//   }
-// }
-
 require('dotenv').config()
 
 module.exports = {
