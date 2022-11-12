3dca99c1757d41550d6db0cc25045dd4a5a589c7
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2022-11-11 22:21:20 -0300

    validando email vazio

--- project/src/domain/entities/usuario.js
+++ project/src/domain/entities/usuario.js
@@ -3,10 +3,18 @@ const { entity, field } = require('@herbsjs/herbs')
 const Usuario =
         entity('Usuario', {
             id: field(Number),
-            email: field(String),
-            nome: field(String),
-            senha: field(String),
-            telefone: field(String),
+            email: field(String, {
+                validation: { presence: true }
+            }),
+            nome: field(String, {
+                validation: { presence: true }
+            }),
+            senha: field(String, {
+                validation: { presence: true }
+            }),
+            telefone: field(String, {
+                validation: { presence: true }
+            }),
         })
 
 module.exports = Usuario
